b0aa3e19695e55215c2bcf5ed547b62a66e6da53
anilrasani <161148310+anilrasani@users.noreply.github.com>
2025-03-22 16:55:07 +0530

    Update data_collection.py

--- finlang/src/data_collection.py
+++ finlang/src/data_collection.py
@@ -1,2 +1,13 @@
-Api_key='finance news and few more apikeys '
+import finnhub
+import requests
+
+Api_key='cpg1ma1r01ql1vn3fk4gcpg1ma1r01ql1vn3fk50'
 print(Api_key)
+URL = "https://finnhub.io/api/v1/news?category=general&token=" + API_KEY
+
+response=requests.get(url)
+if response.status==200:
+  data=response.json()
+  print(data[:3])
+else:
+  print('Error on retriving the data',response.status,response.text)

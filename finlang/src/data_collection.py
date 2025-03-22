86188c54f5e096f86d5a15066367b3b714f5617c
anilrasani <161148310+anilrasani@users.noreply.github.com>
2025-03-22 16:58:26 +0530

    Update data_collection.py

--- finlang/src/data_collection.py
+++ finlang/src/data_collection.py
@@ -8,6 +8,8 @@ URL = "https://finnhub.io/api/v1/news?category=general&token=" + API_KEY
 response=requests.get(url)
 if response.status==200:
   data=response.json()
-  print(data[:3])
+  with open('src/data/financial_news.json','w') as file:
+    json.dump(data,file,indent=4)
+  print('Fianancial news data saved to src/data/financial_news.json')
 else:
   print('Error on retriving the data',response.status,response.text)

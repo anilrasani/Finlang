1948e99a0cee90fa161d0f5629964dd6671607dc
Rasani Anilkumar <rasanianilkumar123@gmail.com>
2025-04-14 11:48:48 +0530

    few changes

---
+++ notebooks/.ipynb_checkpoints/eda-checkpoint.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+import pandas as pd 
+raw_data=pd.read_csv(r'C:\Users\Hi\Finlang\data\cleaned_data.csv')
+raw_data.head(5)
+#1.plot Close over time
+
+plt.plot(raw_data['close'],label='Close')
+plt.ylabel('Close')
+plt.xlabel('Time')
+plt.title('Close over time')
+# plt.show()
+#2.Returns
+    # return = (todays price-yesterday price)/yesterday price
+    
+    # Daily_returns=raw_data['close'].pct_change()
+raw_data['Daily_returns']=raw_data['close'].pct_change()
+# print(raw_data['Daily_returns'])
+
+#3.Volatile
+
+raw_data['volatile']=raw_data['Daily_returns'].std()
+# raw_data['volatile']
+raw_data['rolling_volatile']=raw_data['Daily_returns'].rolling(window=20).std()
+#raw_data['rolling_volatile']
+#4.MA/Support/Resistance
+
+plt.figure(figsize=(12,7))
+plt.plot(raw_data.index,raw_data['rolling_volatile'],color="purple")
+plt.title('Price stability')
+plt.xlabel('time')
+plt.ylabel('volatility')
+plt.grid(True)
+
+plt.show()
+
+

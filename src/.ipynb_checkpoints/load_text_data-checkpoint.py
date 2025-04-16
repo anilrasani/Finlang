0b9d901b1fdc4412489d54de46a374a8701d60d4
Rasani Anilkumar <rasanianilkumar123@gmail.com>
2025-04-16 18:26:43 +0530

    few changes

--- src/.ipynb_checkpoints/load_text_data-checkpoint.py
+++ src/.ipynb_checkpoints/load_text_data-checkpoint.py
@@ -3,10 +3,15 @@ import pandas as pd
 
 # News_apikey='e1827b9251d24621a7d433dbb8d8a27c'
 # Alpha Vantage_API key= "C3AT8EGH2M1612S5"
-finnhub='cpg1ma1r01ql1vn3fk4gcpg1ma1r01ql1vn3fk50'
+# finnhub='cpg1ma1r01ql1vn3fk4gcpg1ma1r01ql1vn3fk50'
 
-data=pd.read_csv(r'C:\Users\Hi\Finlang\data\a.us.txt')
+
+data=pd.read_csv(r'C:\Users\Hi\Finlang\data\kaggle\Cryptocurrency Prices Dataset.csv')
 print(data.head(5))
+
+data1=pd.read_csv(r'C:\Users\Hi\Finlang\data\kaggle\Employee_Earnings_Report_2016.csv')
+print(data1.head(5))
+
 
 
 
--- src/load_text_data.py
+++ src/load_text_data.py
@@ -5,8 +5,13 @@ import pandas as pd
 # Alpha Vantage_API key= "C3AT8EGH2M1612S5"
 # finnhub='cpg1ma1r01ql1vn3fk4gcpg1ma1r01ql1vn3fk50'
 
-data=pd.read_csv(r'C:\Users\Hi\Finlang\data\a.us.txt')
+
+data=pd.read_csv(r'C:\Users\Hi\Finlang\data\kaggle\Cryptocurrency Prices Dataset.csv')
 print(data.head(5))
+
+data1=pd.read_csv(r'C:\Users\Hi\Finlang\data\kaggle\Employee_Earnings_Report_2016.csv')
+print(data1.head(5))
+
 
 
 

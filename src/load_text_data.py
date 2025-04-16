969beb5b01536f70c82bf223e530d2a180870f97
Rasani Anilkumar <rasanianilkumar123@gmail.com>
2025-04-16 17:27:29 +0530

    Added data

---
+++ src/load_text_data.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+# News_apikey='e1827b9251d24621a7d433dbb8d8a27c'
+# Alpha Vantage_API key= "C3AT8EGH2M1612S5"
+# finnhub='cpg1ma1r01ql1vn3fk4gcpg1ma1r01ql1vn3fk50'
+
+data=pd.read_csv(r'C:\Users\Hi\Finlang\data\a.us.txt')
+print(data.head(5))
+
+
+
